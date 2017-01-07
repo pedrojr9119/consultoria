1f73682e291bfcabda7669d21011c60f4e3970db
pedrojr9119 <pedrojr.19@hotmail.com>
2017-01-07 16:14:33 -0200

    Initial commit

---
+++ consultoria/src/main/java/br/com/apr/sistema/ConsultoriaApplication.java
@@ -0,0 +1,12 @@
+package br.com.apr.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultoriaApplication.class, args);
+	}
+}
